d1baa1dc453dbf790773a039e9c4d5c27d635bb8
Hari Om <hari333333om@gmail.com>
2024-06-21 14:35:02 +0530

    Initial Commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import "./Navbar.css";
+import logo from "../../assets/logo.svg";
+
+export default function Navbar() {
+  return (
+    <div className="navbar">
+      <img src={logo} alt="llogo" />
+      <ul className="nav-menu">
+        <li>Home</li>
+        <li>About Me</li>
+        <li>Services</li>
+        <li>Portfolio</li>
+        <li>Contact</li>
+      </ul>
+      <div className="nav-connect">Connect With Me!</div>
+    </div>
+  );
+}
